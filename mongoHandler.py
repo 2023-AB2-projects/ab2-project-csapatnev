f9353795147931997163d3eaaf9f2ebd9392959f
Gall-David <galldavid0120@gmail.com>
2023-03-22 18:43:14 +0200

    mongohandler-insert into + delete from + connect

--- mongoHandler.py
+++ mongoHandler.py
@@ -1,87 +1,70 @@
 from pymongo import MongoClient
-import commands as cmd
-import xml
-from xml import ElementTree
+#import commands as cmd
+from lxml import etree
 
-def find_table_structure(xml_root ,db_name, table_name):
-    # find the table we want
-    table_to_find = xml_root.find(".//Database[@name='{}']/Tables/Table[@name='{}']"
-                                    .format(db_name, table_name))
-
-    return table_to_find
-    
-def handle_my_mongo(current_db_name):
-    current_db_name = current_db_name.upper()
-    
-    # Fetch the xml file
-    xml_root = cmd.parse_xml_file(cmd.XML_FILE_LOCATION)
-
-    if not cmd.database_exists(xml_root, current_db_name):
-        return (-1, f"Error: Database {current_db_name} does not exist!")
+def connect_mongo(db_name):
+    if db_name == "":
+        db_name = "MASTER"
+        client = MongoClient("mongodb://localhost:27017/")
+        db = client[db_name]
+        return db, client
     else:
-        # connect to mongo
-        client = MongoClient("mongodb://localhost:6970/")
-        db = client[current_db_name]
-        return db, xml_root
+        client = MongoClient("mongodb://localhost:27017/")
+        db = client[db_name]
+        return db, client
+
+# When these functions are called, parameters have already been checked for correctness
+def drop_database(db_name, mongoclient):
+    mongoclient.drop_database(db_name)
+
+def drop_table(table_name, mongodb):
+    collection = mongodb[table_name]
+    collection.drop()
+
+def insert_into(mongodb, table_name, primary_key_column, columns, values):
+    collection = mongodb[table_name]
+    primary_key = values[columns.index(primary_key_column)]
+    
+    # Create new lists without the primary_key_column
+    non_primary_columns = [col for col in columns if col != primary_key_column]
+    non_primary_values = [val for col, val in zip(columns, values) if col != primary_key_column]
+    
+    # Create a dictionary of column name to value
+    data = {primary_key_column: primary_key, "value": "#".join(str(value) for value in non_primary_values)}
+    
+    # Insert the data into the collection
+    result = collection.insert_one(data)
+    
+    # Check if the operation was successful
+    if result.inserted_id:
+        return True, f"Document inserted with ID: {result.inserted_id}"
+    else:
+        return False, "Error inserting document"
 
 
-def validate_data_types(table_structure, columns, values):
-    # Get the list of attributes from the table structure
-    attributes = table_structure.findall('Structure/Attribute')
 
-    # Create a dictionary of column name to attribute type
-    column_types = {attr.get('attributeName'): attr.get('type') for attr in attributes}
+# ###
+# SQL: DELETE FROM students WHERE StudID > 10000;
+# # Example usage:
+# db_name = "my_database"
+# table_name = "students"
+# filter_conditions = {"StudID": {"$gt": 10000}}
 
-    # Iterate through the provided columns and values
-    for column, value in zip(columns, values):
-        expected_type = column_types[column]
+# mongodb, client = connect_mongo(db_name)
+# success, message = delete(mongodb, table_name, filter_conditions)
+# print(message)
+###
 
-        # Validate the value based on the expected data type
-        if expected_type == 'VARCHAR':
-            if not isinstance(value, str):
-                return False
-        elif expected_type == 'INT':
-            if not (isinstance(value, int) or value.isdigit()):
-                return False
-        elif expected_type == 'FLOAT':
-            try:
-                float(value)
-            except ValueError:
-                return False
-        elif expected_type == 'BIT':
-            if not (isinstance(value, bool) or value == '0' or value == '1'):
-                return False
-        elif expected_type == 'DATE':
-            if not (isinstance(value, str) and value.count('-') == 2):
-                return False
-        elif expected_type == 'DATETIME':
-            if not (isinstance(value, str) and value.count('-') == 2 and value.count(':') == 2):
-                return False
-        else:
-            # For any other data types, return False
-            return False
+def delete_from(mongodb, table_name, filter_conditions):
+    collection = mongodb[table_name]
+    result = collection.delete_many(filter_conditions)
+    if result.deleted_count > 0:
+        return True, f"Deleted {result.deleted_count} document(s) matching the filter conditions."
+    else:
+        return False, "No document found matching the filter conditions."
 
-    # If all values pass validation, return True
-    return True
-
-
-def insert_into(db_name, table_name, columns, values):
-    db, xml_root = handle_my_mongo(db_name)
-    table_name = table_name.upper()
-    table_structure = find_table_structure(xml_root, db_name, table_name)
-
-
-    # Validate the data types
-    if not validate_data_types(table_structure, columns, values):
-        return (-1, "Error: Invalid data type!")
-
-    pk_index = 0    # every table has a primary key that is the first column
-
-    key = int(values[pk_index])
-    value_list = [values[i] for i in range(len(columns)) if i != pk_index]
-    value = '#'.join(value_list)
-    document = {"Key": key, "Value": value}
-
-    # insert into mongo
-    collection = db[table_name]
-    collection.insert_one(document)
+# testing function
+def fetch_all_documents(mongodb, table_name):
+    collection = mongodb[table_name]
+    documents = collection.find()
+    return list(documents)
